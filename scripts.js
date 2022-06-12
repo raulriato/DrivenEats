16f140cc2ad5d9c311760ce999d7e0ec1d6d6431
raulriato <raulriato@gmail.com>
2022-06-12 13:40:35 -0300

    style js commented

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-let pickedMain, pickedDrink, pickedDessert;
-let priceMain, priceDrink, priceDessert
+let pickedMain, pickedDrink, pickedDessert; //global variables to store the name of the item choosen by the customer
+let priceMain, priceDrink, priceDessert //global variables to store the price of the item choosen by the customer
 
+//This function will show the client the main meal they're picking, in addition to storing the item name and price
 function selectMain(item){
     let picked = document.querySelector('.main .border');
     //console.log(picked);
@@ -10,23 +11,17 @@ function selectMain(item){
         //console.log(document.querySelector('.border .hidden'))
         picked.classList.remove('border');
     }
-    
-    if (picked !== item){
-        item.classList.add('border');
-        document.querySelector('.main .border .check').classList.remove('hidden')
-        pickedMain = document.querySelector('.main .border .name').innerHTML
-        priceMain = Number(document.querySelector('.main .border .price').innerHTML)
-    } else {
-        pickedMain = null
-    }
-    
-   
+    item.classList.toggle('border');
+    document.querySelector('.main .border .check').classList.toggle('hidden')
+    pickedMain = document.querySelector('.main .border .name').innerHTML
+    priceMain = Number(document.querySelector('.main .border .price').innerHTML)
     console.log(pickedMain)
     console.log(priceMain)
 
     allowOrder();
 }
 
+//This function will show the client the drink they're picking, in addition to storing the item name and price
 function selectDrink(item){
     let picked = document.querySelector('.drink .border');
     //console.log(picked);
@@ -36,14 +31,8 @@ function selectDrink(item){
         //console.log(document.querySelector('.border .hidden'))
         picked.classList.remove('border');
     }
-    
-    if (picked !== item){
-        item.classList.add('border');
-        document.querySelector('.drink .border .check').classList.remove('hidden')
-    } else {
-        pickedDrink = null
-    }
-    
+    item.classList.toggle('border');
+    document.querySelector('.drink .border .check').classList.toggle('hidden')
     pickedDrink = document.querySelector('.drink .border .name').innerHTML
     priceDrink = Number(document.querySelector('.drink .border .price').innerHTML)
     console.log(pickedDrink)
@@ -52,6 +41,7 @@ function selectDrink(item){
     allowOrder();
 }
 
+//This function will show the client the dessert they're picking, in addition to storing the item name and price
 function selectDessert(item){
     let picked = document.querySelector('.dessert .border');
     if (picked !== null){
@@ -60,14 +50,8 @@ function selectDessert(item){
         //console.log(document.querySelector('.border .hidden'))
         picked.classList.remove('border');
     }
-
-    if (picked !== item){
-        item.classList.add('border');
-        document.querySelector('.dessert .border .check').classList.remove('hidden');
-    } else {
-        pickedDessert = null
-    }
-
+    item.classList.toggle('border');
+    document.querySelector('.dessert .border .check').classList.toggle('hidden');
     pickedDessert = document.querySelector('.dessert .border .name').innerHTML
     priceDessert = Number(document.querySelector('.dessert .border .price').innerHTML)
     console.log(pickedDessert)
@@ -76,13 +60,10 @@ function selectDessert(item){
     allowOrder();
 }
 
+//Here it's changed the appearance of the order button once the customer picked one of each category
 function allowOrder(){
     if (pickedMain && pickedDrink && pickedDessert){
         document.querySelector('.order').classList.add('allowed')
         console.log(document.querySelector('.order'))
     }
-}
-/* A partir daqui entra a função que vai codificar o pedido pra enviar por Whatsapp */
-function Order(){
-    minhaString = `Olá, gostaria de fazer o pedido:\n`
 }
